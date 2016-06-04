7ba87b7aba2ca45d3aa4a6483df179779e3d0c5c
cherishhsieh <xieqin1991@gmail.com>
2016-06-04 14:48:44 +0800

    add browser check

--- cherish-client.js
+++ cherish-client.js
@@ -44,7 +44,53 @@ var client = function () {
     ps: false
   }
 
+  //检测呈现引擎和浏览器
   var ua = navigator.userAgent
+  if (window.opera) {
+    engine.ver = browser.ver = window.opera.version()
+    engine.opera = browser.opera = parseFloat(engine.ver)
+  } else if (/AppleWebkit\/(\S+)/.test(ua)) {
+    engine.ver = RegExp["$1"]
+    engine.webkit = parseFloat(engine.ver)
+
+    //确定是Chrome还是Safari
+    if (/Chrome\/(\S+)/.test(ua)) {
+      browser.ver = RegExp["$1"]
+      browser.chrome = parseFloat(browser.ver)
+    } else if (/Version\/(\S+)/.test(ua)) {
+      browser.ver = RegExp["$1"]
+      browser.safari = parseFloat(browser.ver)
+    } else {
+      //近似确定版本号
+      var safariVersion = 1
+      if(engine.webkit < 100) {
+        safariVersion = 1
+      } else if (engine.webkit < 312) {
+        safariVersion = 1.2
+      } else if (engine.webkit < 412) {
+        safariVersion = 1.3
+      } else {
+        safariVersion = 2
+      }
+
+      browser.safari = browser.ver = safariVersion
+    }
+  } else if (/KHTML\/(\S+)/.test(ua) || /Konqueror\/([^;]+)/.test(ua)) {
+    engine.ver = browser.ver = RegExp["$1"]
+    engine.khtml = browser.konq = parseFloat(engine.ver)
+  } else if (/rv:([^\)]+)\) Gecko\/d{8}/.test(ua)) {
+    engine.ver = RegExp["$1"]
+    engine.gecko = parseFloat(engine.ver)
+
+    //确定是不是Firefox
+    if (/Firefox\/(\S+)/.test(ua)) {
+      browser.ver = RegExp["$1"]
+      browser.firefox = parseFloat(browser.ver)
+    }
+  } else if (/MSIE ([^;]+)/.test(ua)) {
+    engine.ver = browser.ver = RegExp["$1"]
+    engine.ie = browser.ie = parseFloat(engine.ver)
+  }
 
   return {
     engine: engine,
